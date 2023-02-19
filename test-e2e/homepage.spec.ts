929040211d27fdbefd3aa3785c2b720f66a6de1f
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-02-19 18:41:53 -0500

    Setup e2e test with mocks and example

---
+++ test-e2e/homepage.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/ARchaeology/);
+});
+
+test('get heading', async ({ page }) => {
+  await page.goto('/');
+
+  const heading = page.locator("h1");
+  await expect(heading).toContainText("ARchaeology");
+});
