82ec665a9171bcaafe6868dbaa4336ccea8d7ce1
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-02-19 19:11:12 -0500

    Configure Tailwind css

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to ARchaeology</h1>
+      <h1 className="text-green-500">Welcome to ARchaeology</h1>
     </div>
   );
 }
