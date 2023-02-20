f6b4c6d36086ac5433deb64a4311bc4a6582b64e
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-02-20 09:53:03 -0500

    Fix formatting on tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{ts,tsx,js,jsx}",
-    "./stories/**/*.{ts,tsx,js,jsx,mdx}",
-  ],
+  content: ["./app/**/*.{ts,tsx,js,jsx}", "./stories/**/*.{ts,tsx,js,jsx,mdx}"],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
   ],
 };
