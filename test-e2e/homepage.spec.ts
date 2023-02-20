3ed4f6cfb79aa16c3effb0289ae3c290577777b9
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-02-20 09:56:57 -0500

    Update e2e test

--- test-e2e/homepage.spec.ts
+++ test-e2e/homepage.spec.ts
@@ -2,8 +2,6 @@ import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("/");
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/ARchaeology/);
 });
 
@@ -11,5 +9,5 @@ test("get heading", async ({ page }) => {
   await page.goto("/");
 
   const heading = page.locator("h1");
-  await expect(heading).toContainText("ARchaeology");
+  await expect(heading).toHaveText("Welcome to ARchaeology");
 });
