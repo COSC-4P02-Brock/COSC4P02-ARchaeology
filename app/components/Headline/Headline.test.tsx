8d9c4c4e76a0fdd652ab86b46f0a4d06f24509cc
David Boere <davideboere@gmail.com>
2023-03-22 21:07:40 -0400

    building out headline

--- app/components/Headline/Headline.test.tsx
+++ app/components/Headline/Headline.test.tsx
@@ -2,8 +2,12 @@ import { render, screen } from "@testing-library/react";
 
 import { Headline } from "./Headline";
 
-test("Button renders correctly", () => {
-  const text = "Click me";
-  render(<Headline>{text}</Headline>);
-  expect(screen.getByText(text)).toBeInTheDocument();
+test("Headline renders correctly", () => {
+  const header = "header";
+  const desc = "description";
+  const buttonText = "Click me!";
+  render(<Headline>{header}</Headline>);
+  expect(screen.getByText(header)).toBeInTheDocument();
+  expect(screen.getByText(desc)).toBeInTheDocument();
+  expect(screen.getByText(buttonText)).toBeInTheDocument();
 });
