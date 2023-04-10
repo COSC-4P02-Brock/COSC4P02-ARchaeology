072fe16bd0e4496c18230e8f0e966be5aee4136c
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-04-10 11:09:39 -0400

    Add documentation for pluralize util

--- app/utils/pluralize.ts
+++ app/utils/pluralize.ts
@@ -1,9 +1,18 @@
-export function pluralize({ plural, singular }: {
-  /** The plural form of the things. */
-  plural: string;
+/**
+ * Given a count, return the count and the correct pluralization of the word.
+ * Uses the Canadian English locale for number formatting.
+ */
+export function pluralize(
+  { plural, singular }: {
+    /** The plural form of the things. */
+    plural: string;
 
-  /** The singular form of the things. */
-  singular: string;
-}, count: number /** The number of things. */) {
+    /** The singular form of the things. */
+    singular: string;
+  }, 
+  
+  /** The number of the things. */
+  count: number
+) {
   return `${count.toLocaleString("en-CA")} ${count === 1 ? singular : plural}`;
 }
