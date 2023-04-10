d432845272c608583862be6754a4a2d20447f60e
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-04-10 14:09:58 -0400

    Document some 'magic' going on supabase tests

--- app/utils.server/supabase.server.test.ts
+++ app/utils.server/supabase.server.test.ts
@@ -2,17 +2,26 @@ const TEST_URL = "https://not-so-secret-subdomain.supabase.co";
 const TEST_KEY = "not-so-secret-key";
 
 describe("supabase", () => {
+  // Cache the original values of the environment variables so we can restore
+  // them after the tests run.
   const originalSupabaseUrl = process.env.SUPABASE_URL;
   const originalSupabaseKey = process.env.SUPABASE_KEY;
 
   afterAll(() => {
+    // Restore the original values of the environment variables.
     process.env.SUPABASE_URL = originalSupabaseUrl;
     process.env.SUPABASE_KEY = originalSupabaseKey;
-
+    
+    // Reset the module registry so the next time the supabase module is
+    // is imported, it will be re-evaluated and the environment variables
+    // will be read again with their original values.
     vi.resetModules();
   });
 
   beforeEach(() => {
+    // Reset the module registry so the next time the supabase module is
+    // imported, it will be re-evaluated and the environment variables will
+    // be read again with the values set in the test.
     vi.resetModules();
   });
 
