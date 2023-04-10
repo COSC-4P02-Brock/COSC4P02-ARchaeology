a556802b490f44fb1c41a3ae6197b38915c8c27e
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-04-10 13:58:08 -0400

    Cleanup supabase tests

--- app/utils.server/supabase.server.test.ts
+++ app/utils.server/supabase.server.test.ts
@@ -1,10 +1,13 @@
-describe("subabase", () => {
-  let originalSupbaseUrl = process.env.SUPABASE_URL;
-  let originalSupbaseKey = process.env.SUPABASE_KEY;
+const TEST_URL = "https://not-so-secret-subdomain.supabase.co";
+const TEST_KEY = "not-so-secret-key";
+
+describe("supabase", () => {
+  let originalSupabaseUrl = process.env.SUPABASE_URL;
+  let originalSupabaseKey = process.env.SUPABASE_KEY;
 
   afterAll(() => {
-    process.env.SUPABASE_URL = originalSupbaseUrl;
-    process.env.SUPABASE_KEY = originalSupbaseKey;
+    process.env.SUPABASE_URL = originalSupabaseUrl;
+    process.env.SUPABASE_KEY = originalSupabaseKey;
 
     vi.resetModules();
   });
@@ -15,21 +18,21 @@ describe("subabase", () => {
 
   test("throws an error if SUPABASE_URL env var is not set", async () => {
     process.env.SUPABASE_URL = "";
-    process.env.SUPABASE_KEY = "notsosecretkey";
+    process.env.SUPABASE_KEY = TEST_KEY;
 
     await expect(import("./supabase.server")).rejects.toThrowError();
   });
 
   test("throws an error if SUPABASE_KEY env var is not set", async () => {
-    process.env.SUPABASE_URL = "https://notsosecretsubdomain.supabase.co";
+    process.env.SUPABASE_URL = TEST_URL;
     process.env.SUPABASE_KEY = "";
 
     await expect(import("./supabase.server")).rejects.toThrowError();
   });
 
   test("imports supabase", async () => {
-    process.env.SUPABASE_URL = "https://notsosecretsubdomain.supabase.co";
-    process.env.SUPABASE_KEY = "notsosecretkey";
+    process.env.SUPABASE_URL = TEST_URL;
+    process.env.SUPABASE_KEY = TEST_KEY;
 
     const { supabase } = await import("./supabase.server");
     expect(supabase).toBeDefined();
