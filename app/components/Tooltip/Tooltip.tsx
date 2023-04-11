b2620dc395557d1eb20b6eed4bd341b438f229a7
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-04-10 23:50:16 -0400

    Ensure tooltip has styles in dev

--- app/components/Tooltip/Tooltip.tsx
+++ app/components/Tooltip/Tooltip.tsx
@@ -1,6 +1,8 @@
 import Tippy from "@tippyjs/react";
 import type { TippyProps } from "@tippyjs/react";
 
+import "tippy.js/dist/tippy.css";
+
 type TooltipProps = Pick<TippyProps, "content" | "children">;
 
 export const Tooltip = ({ children, content }: TooltipProps) => (
