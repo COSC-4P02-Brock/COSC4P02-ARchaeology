e6de392e40a4fbaa6bbe58f0b5b96f308841aabd
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-04-11 20:22:41 -0400

    Update tests

--- app/utils.server/supabase.server.test.ts
+++ app/utils.server/supabase.server.test.ts
@@ -1,49 +1,11 @@
+import { supabase } from "./supabase.server";
+
 const TEST_URL = "https://not-so-secret-subdomain.supabase.co";
 const TEST_KEY = "not-so-secret-key";
 
 describe("supabase", () => {
-  // Cache the original values of the environment variables so we can restore
-  // them after the tests run.
-  const originalSupabaseUrl = process.env.SUPABASE_URL;
-  const originalSupabaseKey = process.env.SUPABASE_KEY;
-
-  afterAll(() => {
-    // Restore the original values of the environment variables.
-    process.env.SUPABASE_URL = originalSupabaseUrl;
-    process.env.SUPABASE_KEY = originalSupabaseKey;
-    
-    // Reset the module registry so the next time the supabase module is
-    // is imported, it will be re-evaluated and the environment variables
-    // will be read again with their original values.
-    vi.resetModules();
-  });
-
-  beforeEach(() => {
-    // Reset the module registry so the next time the supabase module is
-    // imported, it will be re-evaluated and the environment variables will
-    // be read again with the values set in the test.
-    vi.resetModules();
-  });
-
-  test("throws an error if SUPABASE_URL env var is not set", async () => {
-    process.env.SUPABASE_URL = "";
-    process.env.SUPABASE_KEY = TEST_KEY;
-
-    await expect(import("./supabase.server")).rejects.toThrowError();
-  });
-
-  test("throws an error if SUPABASE_KEY env var is not set", async () => {
-    process.env.SUPABASE_URL = TEST_URL;
-    process.env.SUPABASE_KEY = "";
-
-    await expect(import("./supabase.server")).rejects.toThrowError();
-  });
-
-  test("imports supabase", async () => {
-    process.env.SUPABASE_URL = TEST_URL;
-    process.env.SUPABASE_KEY = TEST_KEY;
-
-    const { supabase } = await import("./supabase.server");
-    expect(supabase).toBeDefined();
+  test("creates supabase client", () => {
+    const client = supabase({ SUPABASE_URL: TEST_URL, SUPABASE_KEY: TEST_KEY });
+    expect(client).toBeDefined();
   });
 });
