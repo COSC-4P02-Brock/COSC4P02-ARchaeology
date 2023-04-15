99b02ad166b24046ab5aa6bbd956b0a6fc2cdd51
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-04-15 10:48:19 -0400

    Only logout on GET request

--- app/routes/sign-out.tsx
+++ app/routes/sign-out.tsx
@@ -1,15 +1,7 @@
-import type { ActionArgs, LoaderArgs } from "@remix-run/cloudflare";
+import type { LoaderArgs } from "@remix-run/cloudflare";
 import { redirect } from "@remix-run/cloudflare";
 
 import { destroySession } from "../utils.server";
-
-export const action = async ({ context, request }: ActionArgs) => {
-  return redirect("/sign-in", {
-    headers: {
-      "Set-Cookie": await destroySession(request, context),
-    },
-  });
-}
 
 export const loader = async ({ context, request }: LoaderArgs) => {
   return redirect("/sign-in", {
