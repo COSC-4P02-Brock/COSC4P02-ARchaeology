a630fe8ac044f4c0d1ea57ffcaf61a97d7f36f04
rworron <rw11dt@brocku.ca>
2023-04-24 01:41:08 -0400

    styled page for the list of artifacts

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -19,16 +19,16 @@ export const Header = ({
   <header className="" aria-labelledby="header-heading">
     <h2 id="header-heading" className="sr-only">Header</h2>
 
-    <div className="grid grid-cols-9 grid-rows-1">
+    <div className="mt-4 ml-4 mr-4 grid grid-cols-2 grid-rows-1">
       <div><a href={url} title={`Visit the ${name} website`}>
         <Logo theme="light" />
       </a> </div>
       
 
-      <div className="mt-12 -mb-6 flex justify-right space-x-4 sm:space-x-12">
+      <div className="flex justify-end mt-12 mb-6 space-x-4 sm:space-x-12">
         {headerMenuLinks.map(({ name, url }) => (
             <div className="pb-6" key={`${name}.${url}`}>
-              <a className="text-sm leading-6 text-black hover:text-grey-300"
+              <a className="text-lg leading-6 text-black hover:text-red-700"
                  data-testid="Header.HeaderMenuLink"
                  href={url}
               >
--- app/routes/__public.tsx
+++ app/routes/__public.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "@remix-run/react";
 
+import { Header } from "../components/Header"
 import { Footer } from "../components/Footer";
 import { MuseumSiteInfo } from "../models";
 
@@ -9,6 +10,7 @@ const year = today.getFullYear().toString();
 export default function Layout() {
   return (
     <>
+      <Header currentYear={year} siteInfo={MuseumSiteInfo}/>
       <main className="grow w-full mx-auto max-w-7xl sm:px-6 lg:px-8 py-6 lg:py-8">
         <Outlet />
       </main>
